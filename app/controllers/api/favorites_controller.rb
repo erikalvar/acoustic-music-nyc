e2cebbea820a04d870895cf791fd5bd192e1d3b6
erikalvar <erikalvarbass@gmail.com>
2020-07-31 15:42:55 -0400

    adds already favorited

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,6 +1,11 @@
 class Api::FavoritesController < ApplicationController
 
   before_action :authenticate_user
+
+  def already_favorited?
+    Favorite.where(user_id: current_user.id, event_id:
+    params[:event_id]).exists?
+  end
   
   def index
     @favorites = Favorite.all
@@ -8,14 +13,18 @@ class Api::FavoritesController < ApplicationController
   end
 
   def create
-    @favorite = Favorite.new(
-      user_id: current_user.id,
-      event_id: params[:event_id],
-    )
-    if @favorite.save
-      render "show.json.jb"
+    if already_favorited?
+      render json: {message: "alreaded favorited"}
     else
-      render json: {errors: @favorite.errors.full_messages}, status: :unprocessable_entity
+      @favorite = Favorite.new(
+        user_id: current_user.id,
+        event_id: params[:event_id],
+      )
+      if @favorite.save
+        render "show.json.jb"
+      else
+        render json: {errors: @favorite.errors.full_messages}, status: :unprocessable_entity
+      end
     end
   end
 
@@ -29,9 +38,13 @@ class Api::FavoritesController < ApplicationController
   end
 
   def destroy
-    @favorite = Favorite.find_by(id: params[:id])
-    @favorite.destroy
-    render json: {message: "favorite successfully deleted."}
+    if !(already_favorited?)
+      render json: {message: "not yet favorited"}
+    else
+      @favorite = Favorite.find_by(id: params[:id])
+      @favorite.destroy
+      render json: {message: "favorite successfully deleted."}
+    end
   end
 
 end
