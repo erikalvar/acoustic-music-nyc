90d7c125dc758e8cd9aa566a7c8df98427eb0d98
erikalvar <erikalvarbass@gmail.com>
2020-08-06 11:36:22 -0400

    updates cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'example.com', 'https://elated-pare-4c0f41.netlify.app/'
+    origins 'localhost:8080', 'https://elated-pare-4c0f41.netlify.app/'
 
     resource '*',
       headers: :any,
