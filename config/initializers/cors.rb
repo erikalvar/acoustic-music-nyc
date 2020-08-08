e0257df4e6fcb2682a2f62174b8afa1583c95dc9
erikalvar <erikalvarbass@gmail.com>
2020-08-08 11:28:45 -0400

    adds custom domain in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'https://heuristic-pare-ae9758.netlify.app'
+    origins 'localhost:8080', 'https://heuristic-pare-ae9758.netlify.app', 'https://www.acousticmusicnyc.com'
 
     resource '*',
       headers: :any,
